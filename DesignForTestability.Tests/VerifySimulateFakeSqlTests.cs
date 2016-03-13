fca1002f842025ee4e17a025fafccaf8129d37b4
William Li <wul@microsoft.com>
2016-03-13 15:33:41 -0700

    add verify simulator tests

--- DesignForTestability.Tests/VerifySimulateFakeSqlTests.cs
+++ DesignForTestability.Tests/VerifySimulateFakeSqlTests.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace designIssueExample.Tests
 {
-    class VerifySimulateFakeSqlTests
+    [TestFixture]
+    internal class VerifySimulateFakeSqlTests
     {
+        [Test]
+        public void GivenWhatRepositoryGetFromFakeSql_QueryViaRepository_ExpectResultIsTheSame()
+        {
+            var employeeRepositoryWithFakeSql = new EmployeeRepository(new FakeSqlDriver());
+            var fakeSqlResult = employeeRepositoryWithFakeSql.GetEmployees(EmployeeFilterType.ByName, "T");
+            var fakeSqlInternalData = fakeSqlResult.Select(employee => new object[] {employee.Id, employee.Name, employee.Age, employee.IsSalaried}).ToList();
+
+            var simulateFakeSqlDriver = new SimulateFakeSqlDriver();
+            simulateFakeSqlDriver.SimulatorReplaceData(fakeSqlInternalData);
+
+            var employeeRepositoryWithSimulator= new EmployeeRepository(simulateFakeSqlDriver);
+            var simulatorResult = employeeRepositoryWithSimulator.GetEmployees(EmployeeFilterType.ByName, "T");
+
+            Assert.AreEqual(JustmakeThemAllString(simulatorResult), JustmakeThemAllString(fakeSqlResult));
+        }
+
+        private static string JustmakeThemAllString(IEnumerable<Employee> fakeSqlResult)
+        {
+            return fakeSqlResult.Aggregate("", (current, employee) => current + (employee + "\n"));
+        }
     }
 }

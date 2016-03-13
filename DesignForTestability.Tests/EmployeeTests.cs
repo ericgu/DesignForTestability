1cd659b594cce471bd27bde23af20ac806059510
William Li <wul@microsoft.com>
2016-03-13 16:01:11 -0700

    add buessiness logic test

---
+++ DesignForTestability.Tests/EmployeeTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace designIssueExample.Tests
+{
+    [TestFixture]
+    class EmployeeTests
+    {
+        [Test]
+        // LOL the only domain test
+        public void GivenEmployeeData_ConvertToString_ExpectFormatIsCorrect()
+        {
+            // Arrange
+            var e = new Employee {Age = 42, Id = 4242, IsSalaried = true, Name = "Hue Hue"};
+            // Act
+            var result = e.ToString();
+            // Assert
+            Assert.AreEqual(result, "4242 Hue Hue 42 True");
+        }
+    }
+}

2d06c321a69d7372d4d12b51dc8f973ab636a75a
Eric Gunnerson <eric_gunnerson@hotmail.com>
2016-03-14 21:35:15 -0700

    Added exempt test for too young

--- designIssueExampleTests/EmployeeFilter_Tests.cs
+++ designIssueExampleTests/EmployeeFilter_Tests.cs
@@ -35,5 +35,13 @@ namespace designIssueExampleTests
 
             Assert.IsTrue(employeeFilter.Matches("Alan", 42, false));
         }
+
+        [TestMethod]
+        public void When_I_create_a_exempt_filter_with_an_age_that_is_too_young__Matches_returns_false()
+        {
+            EmployeeFilter employeeFilter = new EmployeeFilter(EmployeeFilterType.ExemptOnly, null);
+
+            Assert.IsFalse(employeeFilter.Matches("Alan", 39, false));
+        }
     }
 }

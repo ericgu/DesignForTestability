a587bf55d50234642952c65ef3f895daac2416a0
Eric Gunnerson <eric_gunnerson@hotmail.com>
2016-03-16 20:43:43 -0700

    R: Changed CreateEmployeeCollection signature to take an EmployeeCollection parameter

--- EmployeeCollection.cs
+++ EmployeeCollection.cs
@@ -4,7 +4,7 @@ namespace designIssueExample
 {
     class EmployeeCollection
     {
-        public static List<Employee> CreateEmployeeCollection()
+        public static List<Employee> CreateEmployeeCollection(EmployeeCollection employeeCollection)
         {
             List<Employee> result = new List<Employee>();
             return result;
--- Yucky.cs
+++ Yucky.cs
@@ -14,7 +14,7 @@ namespace designIssueExample
         {
             string query = "select * from employee, employee_role inner join employee.Id == employee_role.EmployeeId";
 
-            var result = EmployeeCollection.CreateEmployeeCollection();
+            var result = EmployeeCollection.CreateEmployeeCollection(new EmployeeCollection());
             using (FakeSqlCommand sqlCommand = new FakeSqlCommand(query, connection))
             {
                 FakeSqlDataReader reader;

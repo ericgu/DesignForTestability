3a769df43b86d9c67950569df6e2645cfcb4df9e
Eric Gunnerson <eric_gunnerson@hotmail.com>
2016-03-18 22:18:40 -0700

    R: Move the method to the EmployeeCollection class

--- EmployeeCollection.cs
+++ EmployeeCollection.cs
@@ -15,5 +15,20 @@ namespace designIssueExample
         {
             get { return _employeeCollection; }
         }
+
+        public static void AddEmployeeIfMatch(EmployeeFilter employeeFilter, string name, int age, bool isSalaried,
+            EmployeeCollection employeeCollection, int id)
+        {
+            if (employeeFilter.Matches(name, age, isSalaried))
+            {
+                employeeCollection.Items.Add(new Employee
+                {
+                    Name = name,
+                    Id = id,
+                    Age = age,
+                    IsSalaried = isSalaried
+                });
+            }
+        }
     }
 }

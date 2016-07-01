8ac2cd0c12f5c8fc871c8551be7cb531bc76cf97
Eric Gunnerson <eric_gunnerson@hotmail.com>
2016-06-30 18:19:54 -0700

    Added pipeline class & tests

---
+++ Pipeline.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace designIssueExample
+{
+    public class Pipeline
+    {
+        public static void Process<T1, T2>(Func<T1> source, Func<T1, T2> processor, Action<T2> sink )
+        {
+            sink(processor(source()));
+        }
+    }
+}
---
+++ designIssueExampleTests/Pipeline_Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using designIssueExample;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace designIssueExampleTests
+{
+    [TestClass]
+    public class Pipeline_Tests
+    {
+        [TestMethod]
+        public void When_I_call_Process__it_properly_passes_data_through()
+        {
+            string result = null;
+
+            Pipeline.Process<int, string>(
+                () => 15,
+                (number) => number.ToString(),
+                (s) => { result = s; }
+                );
+
+            Assert.AreEqual("15", result);
+        }
+    }
+}
